7cc27563fd632a753548daec20fbcbafd373a144
Juan Herrera <herrera@parkside.at>
2020-05-13 21:05:48 +0200

    feat: add all features

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@
 const input$ = document.getElementById('word-input');
 const searchButton$ = document.getElementById('search-button');
 const loader$ = document.getElementById('loader');
+const similarWord$ = document.getElementById('similar-word');
 const notFound$ = document.getElementById('not-found');
 const notFoundMessage$ = document.getElementById('not-found-message');
 const serverError$ = document.getElementById('server-error');
@@ -10,57 +11,83 @@ const result$ = document.getElementById('result');
 const title$ = document.getElementById('title');
 const article$ = document.getElementById('article');
 const description$ = document.getElementById('description');
-const credits$ = document.getElementById('credits');
+const link$ = document.getElementById('link');
 const url = 'https://safe-fjord-57072.herokuapp.com/search/';
+let searchTerm = '';
 
 const startSearch = () => {
   loader$.style.display = "block";
   result$.style.display = "none";
   notFound$.style.display = "none";
   serverError$.style.display = "none";
+  similarWord$.style.display = "none";
 }
 
 const endSearch = () => {
   loader$.style.display = "none";
 }
 
+const showDefinition = (data) => {
+  result$.style.display = "block";
+  title$.innerHTML = data.title;
+  if (searchTerm !== data.title.toLowerCase().trim()) {
+    similarWord$.style.display = "block";
+  }
+
+  switch (data.gender.toLowerCase()) {
+    case 'maskulin':
+      article$.innerHTML = 'der';
+      break;
+    case 'feminin':
+      article$.innerHTML = 'die';
+      break;
+    case 'neutrum':
+      article$.innerHTML = 'das';
+      break;
+  }
+  description$.innerHTML = data.description;
+  link$.setAttribute('href', data.link);
+}
+
+const showError = (data) => {
+  switch (data.id) {
+    case 1:
+      notFound$.style.display = "block";
+      break;
+    case 2:
+      notFound$.style.display = "block";
+      break;
+    default:
+      serverError$.style.display = "block";
+      break;
+  }
+}
+
+const showResult = (response) => {
+  response.json().then((data) => {
+    if (!response.ok) {
+      showError(data);
+    } else {
+      showDefinition(data);
+    }
+  });
+}
+
 const searchDefinition = () => {
-  startSearch();
   try {
-    fetch(url + input$.value).then((response) => {
-      response.json().then((data) => {
-        endSearch();
-        if (!response.ok) {
-          switch (data.id) {
-            case 1:
-              notFound$.style.display = "block";
-              break;
-            case 2:
-              notFoundMessage$.innerHTML += ". Make sure it is a noun 😉";
-              notFound$.style.display = "block";
-              break;
-            default:
-              serverError$.style.display = "block";
-              break;
-          }
-        } else {
-          result$.style.display = "block";
-          title$.innerHTML = data.title;
-          switch (data.gender.toLowerCase()) {
-            case 'maskulin':
-              article$.innerHTML = 'der';
-              break;
-            case 'feminin':
-              article$.innerHTML = 'die';
-              break;
-            case 'neutrum':
-              article$.innerHTML = 'das';
-              break;
-          }
-          description$.innerHTML = data.description;
-          credits$.style.display = "block";
-        }
-      })
+    startSearch();
+    searchTerm = input$.value.toLowerCase().trim();
+    fetch(url + searchTerm).then((response) => {
+      console.log(response);
+      endSearch();
+      if (response.status === 404) {
+        throw {};
+      } else {
+        showResult(response);
+      }
+    }).catch(() => {
+      endSearch();
+      serverError$.style.display = "block";
     });
   } catch (error) {
     endSearch();
